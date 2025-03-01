b57140c08c646427d2b8c8557ef91ef1f121f723
Alexander Hendrix <hendrixtothemax38@gmail.com>
2025-02-28 20:49:12 -0700

    moved some game logic

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ print('Welcome to Wordle!')
 maxTurns = 6
 word = 'KIRBY'
 message = "Try and guess today's wordle: "
+guesses = []
 
 #checking if any of the letters are in the correct word
 for turn in range(0,maxTurns):
-    userguess = input(f'{message}', end="")
+    chars=""
+    print(f'{message}', end="")
+    userguess = input()
     userguess = userguess.upper()
     print(f'\r', end="")
     
@@ -17,21 +20,26 @@ for turn in range(0,maxTurns):
         charColor = Fore.WHITE
         charBgColor = Back.RESET
 
-        if guesscharacter.upper() in word:
+        if guesscharacter.upper() in word.upper():
             charBgColor = Back.YELLOW
-            if guesscharacter.upper() == word[i]:
+            if guesscharacter.upper() == word[i].upper():
                 charBgColor = Back.GREEN
                 #print(f'{Fore.RED} Correct Position {guesscharacter} == {word[i]}')
-            
-                if guesscharacter.upper() == word:
-                    charBgColor = Back.GREEN
-                    #print(f'{Fore.RED} Correct Position {guesscharacter} == {word[i]}')
 
-        print(f'{charColor}{charBgColor}{guesscharacter}', end="")
-    print(f'{Back.RESET} {Fore.RESET}', end="")
+        if guesscharacter != " ":
+            chars += f'{charColor}{charBgColor}{guesscharacter}'
+    guesses.append(chars)
+
+    print('\n--------------------')
+    for j,guess in enumerate(guesses):
+        print(f'{guess}')
+    print(f'{Back.RESET}{Fore.RESET}', end="")
+    print(f'--------------------{Back.RESET}{Fore.RESET}')
     message = ""
-    
-        
+    if userguess.upper() == word.upper():
+        break
 
-        
-        
+if userguess.upper() == word.upper():   
+    print("You guessed the word! Good Job!")
+else:
+    print(f"You failed to guess the word! The word was {word}!")
